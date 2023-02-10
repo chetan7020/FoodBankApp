0dce41c36d1ff16fe605f483692a8efe6ead9f48
Sushant <sushantxt123@gmail.com>
2023-02-10 16:10:00 +0530

    some changes

--- app/src/main/java/com/codizcdp/foodbanksurplus/UserLoginActivity.java
+++ app/src/main/java/com/codizcdp/foodbanksurplus/UserLoginActivity.java
@@ -7,10 +7,14 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class UserLoginActivity extends AppCompatActivity {
 
 
+
+    Spinner sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,5 +29,20 @@ public class UserLoginActivity extends AppCompatActivity {
             window.setStatusBarColor(this.getResources().getColor(R.color.theme));
             window.setNavigationBarColor(this.getResources().getColor(R.color.theme));
         }
+
+        sp = findViewById(R.id.loginSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.user_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+//                R.array.user_array, android.R.layout.simple_dropdown_item_1line);
+//
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+// Apply the adapter to the spinner
+        sp.setAdapter(adapter);
+
     }
 }
