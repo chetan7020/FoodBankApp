dda3dff93f9cfbcb5e66e1e20bf7e0e04d837d96
yash aher <aheryash2004@gmail.com>
2023-02-10 18:52:15 +0530

    login screen done

--- app/src/main/java/com/codizcdp/foodbanksurplus/UserLoginActivity.java
+++ app/src/main/java/com/codizcdp/foodbanksurplus/UserLoginActivity.java
@@ -5,44 +5,43 @@ import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class UserLoginActivity extends AppCompatActivity {
 
 
-
-    Spinner sp;
+    TextInputLayout textInputLayout;
+    AutoCompleteTextView autoCompleteTextView;
+    String [] items = {"User","Admin","Provider"};
+    ArrayAdapter<String> adapterItems;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
 
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
-        if (Build.VERSION.SDK_INT >= 21) {
-            Window window = this.getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.setStatusBarColor(this.getResources().getColor(R.color.theme));
-            window.setNavigationBarColor(this.getResources().getColor(R.color.theme));
-        }
+        textInputLayout = findViewById(R.id.menu_drop);
+        autoCompleteTextView = findViewById(R.id.drop_items);
 
-        sp = findViewById(R.id.loginSpinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.user_array, android.R.layout.simple_spinner_item);
+        adapterItems = new ArrayAdapter<String>(this,R.layout.items_list,items);
+        autoCompleteTextView.setAdapter(adapterItems);
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String items = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(UserLoginActivity.this, items, Toast.LENGTH_SHORT).show();
+            }
+        });
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-//                R.array.user_array, android.R.layout.simple_dropdown_item_1line);
-//
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-// Apply the adapter to the spinner
-        sp.setAdapter(adapter);
 
     }
 }
