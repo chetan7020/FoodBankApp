8e13241c52a102dbfb083be4d4535292dbe13983
Sushant <sushantxt123@gmail.com>
2023-02-10 15:28:39 +0530

    some changes

--- app/src/main/java/com/codizcdp/foodbanksurplus/UserLoginActivity.java
+++ app/src/main/java/com/codizcdp/foodbanksurplus/UserLoginActivity.java
@@ -1,8 +1,12 @@
 package com.codizcdp.foodbanksurplus;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class UserLoginActivity extends AppCompatActivity {
 
@@ -11,5 +15,15 @@ public class UserLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.theme));
+            window.setNavigationBarColor(this.getResources().getColor(R.color.theme));
+        }
     }
 }
