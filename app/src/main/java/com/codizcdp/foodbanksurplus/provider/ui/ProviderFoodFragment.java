8a7d66272bf4995c46a40edb26ec4158b3d7b107
TMR <chetandagajipatil333@gmail.com>
2023-02-11 13:13:38 +0530

    provider food form and feedback design is done

--- app/src/main/java/com/codizcdp/foodbanksurplus/provider/ui/ProviderFoodFragment.java
+++ app/src/main/java/com/codizcdp/foodbanksurplus/provider/ui/ProviderFoodFragment.java
@@ -5,36 +5,47 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.codizcdp.foodbanksurplus.LoginActivity;
 import com.codizcdp.foodbanksurplus.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class ProviderFoodFragment extends Fragment {
 
     private View view;
+    TextInputLayout textInputLayout;
+    AutoCompleteTextView autoCompleteTextView;
+    String [] items = {"Cooked Food", "Un-Cooked Food"};
+    ArrayAdapter<String> adapterItems;
 
     private void init() {
         initialize();
+
+        adapterItems = new ArrayAdapter<String>(getActivity(),R.layout.items_list,items);
+        autoCompleteTextView.setAdapter(adapterItems);
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String items = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(getActivity(), items, Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
     private void initialize() {
 
-        FloatingActionButton plusButton;
-        plusButton = view.findViewById(R.id.addFoodFloatingActionBtn);
-
-        plusButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                Chetan, add the logic to load/replace the new fragment of adding new food
-            }
-        });
-
+        textInputLayout = view.findViewById(R.id.menu_drop);
+        autoCompleteTextView = view.findViewById(R.id.drop_items);
     }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
