5e71b4fd766c22d8f6ea6ae212044a2bcad8506f
TMR <chetandagajipatil333@gmail.com>
2023-02-11 12:17:16 +0530

    intent error solved

--- app/src/main/java/com/codizcdp/foodbanksurplus/SplashActivity.java
+++ app/src/main/java/com/codizcdp/foodbanksurplus/SplashActivity.java
@@ -1,13 +1,10 @@
 package com.codizcdp.foodbanksurplus;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
 
-import com.codizcdp.foodbanksurplus.provider.ProviderMainActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -15,30 +12,11 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
-
-        Thread thread = new Thread(){
-
+        new Handler().postDelayed(new Runnable() {
             public void run() {
-                try {
-
-                    sleep(2000);
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-
-                } finally {
-
-                    Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
-                    startActivity(intent);
-                }
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
             }
-
-        };
-        thread.start();
+        }, 1500);
     }
-    public void nxtActivity(View view) {
-        startActivity(new Intent(SplashActivity.this, LoginActivity.class));
-    }
-
 }
