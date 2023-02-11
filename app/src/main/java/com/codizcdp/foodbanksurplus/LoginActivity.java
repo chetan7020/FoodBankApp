4aa5162de084d7ea1a859fd0e360c97d3f6e7bc5
TMR <chetandagajipatil333@gmail.com>
2023-02-11 19:50:06 +0530

    scroll view added in sign in and up

--- app/src/main/java/com/codizcdp/foodbanksurplus/LoginActivity.java
+++ app/src/main/java/com/codizcdp/foodbanksurplus/LoginActivity.java
@@ -15,30 +15,13 @@ import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
-    TextInputLayout textInputLayout;
-    AutoCompleteTextView autoCompleteTextView;
-    String [] items = {"User","Admin","Provider"};
-    ArrayAdapter<String> adapterItems;
-
     TextView register_here;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        textInputLayout = findViewById(R.id.menu_drop);
-        autoCompleteTextView = findViewById(R.id.drop_items);
         register_here = findViewById(R.id.register);
-
-        adapterItems = new ArrayAdapter<String>(this,R.layout.items_list,items);
-        autoCompleteTextView.setAdapter(adapterItems);
-        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String items = adapterView.getItemAtPosition(i).toString();
-                Toast.makeText(LoginActivity.this, items, Toast.LENGTH_SHORT).show();
-            }
-        });
 
         register_here.setOnClickListener(new View.OnClickListener() {
             @Override
