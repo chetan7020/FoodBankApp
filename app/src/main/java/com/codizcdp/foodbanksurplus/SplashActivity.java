ddfb3195568be67ae8119c706e4f6ffd38abd098
Prashant1919 <jagtapprashant2020@gmail.com>
2023-02-11 13:52:15 +0530

    notification added

--- app/src/main/java/com/codizcdp/foodbanksurplus/SplashActivity.java
+++ app/src/main/java/com/codizcdp/foodbanksurplus/SplashActivity.java
@@ -6,12 +6,25 @@ import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SplashActivity extends AppCompatActivity {
+import com.onesignal.OneSignal;
 
+public class SplashActivity extends AppCompatActivity {
+    private static final String ONESIGNAL_APP_ID = "b8ff450c-b28d-4555-8c85-5c56bd36efd9";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        // Enable verbose OneSignal logging to debug issues if needed.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
+
+        // promptForPushNotifications will show the native Android notification permission prompt.
+        // We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 7)
+        OneSignal.promptForPushNotifications();
+
         new Handler().postDelayed(new Runnable() {
             public void run() {
                 startActivity(new Intent(SplashActivity.this, LoginActivity.class));
