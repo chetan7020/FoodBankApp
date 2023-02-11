3076d9fec900f206a679ae3ba4a53d563685f97b
yash aher <aheryash2004@gmail.com>
2023-02-11 09:56:38 +0530

    login screen done

---
+++ app/src/main/java/com/codizcdp/foodbanksurplus/ProviderLoginActivity.java
@@ -0,0 +1,14 @@
+package com.codizcdp.foodbanksurplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProviderLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_provider_login);
+    }
+}
